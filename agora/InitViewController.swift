8547e35ed9410a33477c8600c35420da1cf49c24
jjz <woaf1003@gmail.com>
2017-04-09 21:28:55 +0800

    use Privacy

--- agora/InitViewController.swift
+++ agora/InitViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class InitViewController: UIViewController {
     var agoraKit : AgoraRtcEngineKit!
+    
+    @IBOutlet weak var localVideo: UIView!
+    @IBOutlet weak var remoteView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        initAgoraEngine()
+        setupVideo()
+        setupLocalVideo()
+        joinChannel()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,7 +28,51 @@ class InitViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    func initAgoraEngine(){
+        agoraKit=AgoraRtcEngineKit.sharedEngine(withAppId: AgoraSetting.AgoraAppId, delegate: self)
+    }
+    func setupVideo(){
+        agoraKit.enableVideo()
+        agoraKit.setVideoProfile(._VideoProfile_360P, swapWidthAndHeight: false)
+    }
+    func setupLocalVideo(){
+        let videoCanvas=AgoraRtcVideoCanvas()
+        videoCanvas.uid=0
+        videoCanvas.view=localVideo
+        videoCanvas.renderMode = .render_Adaptive
+        agoraKit.setupLocalVideo(videoCanvas)
+    }
 
+    func joinChannel(){
+        agoraKit.joinChannel(byKey: nil, channelName: "demo", info: nil, uid: 0){[weak self](sid,uid,elapsed)->Void in
+            if let weakSelf = self{
+                weakSelf.agoraKit.setEnableSpeakerphone(true)
+                UIApplication.shared.isIdleTimerDisabled = true
+            }
+            
+    }
+    }
 
 }
 
+extension InitViewController:AgoraRtcEngineDelegate{
+    func rtcEngine(_ engine: AgoraRtcEngineKit!, firstRemoteVideoDecodedOfUid uid: UInt, size: CGSize, elapsed: Int) {
+        if(remoteView.isHidden){
+        remoteView.isHidden=false
+        }
+        let videoCanvas = AgoraRtcVideoCanvas()
+        videoCanvas.uid = uid
+        videoCanvas.view = remoteView
+        videoCanvas.renderMode = .render_Adaptive
+        agoraKit.setupRemoteVideo(videoCanvas)
+
+        
+        
+    }
+    func rtcEngine(_ engine: AgoraRtcEngineKit!, didLeaveChannelWith stats: AgoraRtcStats!) {
+        
+    }
+    func rtcEngine(_ engine: AgoraRtcEngineKit!, didVideoEnabled enabled: Bool, byUid uid: UInt) {
+        
+    }
+}
